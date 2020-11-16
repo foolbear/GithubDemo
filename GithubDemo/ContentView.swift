e75b405a77b45e481150481a9a58534405c273a8
foolbear <greatfoolbear@gmail.com>
2020-11-17 00:39:29 +0800

    ContentView.

--- GithubDemo/ContentView.swift
+++ GithubDemo/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section(header: Text("current_user_url")) {
+                Text("test response")
+                Text("\(viewModel.response.current_user_url)")
+            }
+            Section(header: Text("current_user_authorizations_html_url")) {
+                Text("\(viewModel.response.current_user_authorizations_html_url)")
+            }
+            Section(header: Text("authorizations_url")) {
+                Text("\(viewModel.response.authorizations_url)")
+            }
+            Section(header: Text("...")) {
+                Text("...")
+            }
+        }
     }
 }
 
--- GithubDemo/ViewModel.swift
+++ GithubDemo/ViewModel.swift
@@ -9,19 +9,21 @@ import SwiftUI
 import Combine
 
 final class ViewModel: ObservableObject {
+    let url = "https://api.github.com"
+    let delay: Double = 5.0
     @Published var response: GitHubResponse = GitHubResponse()
     var subscriptions = Set<AnyCancellable>()
     
     func request() {
-        let publisher = URLSession.shared.dataTaskPublisher(for: URL(string: "https://api.github.com")!)
+        let publisher = URLSession.shared.dataTaskPublisher(for: URL(string: url)!)
             .map { $0.data }
             .decode(type: GitHubResponse.self, decoder: JSONDecoder())
             .replaceError(with: GitHubResponse())
             .receive(on: RunLoop.main)
             .print()
-        Timer.publish(every: 5.0, on: .main, in: .default)
+        Timer.publish(every: delay, on: .main, in: .default)
             .autoconnect()
-            .delay(for: .seconds(5.0), scheduler: RunLoop.main, options: .none)
+            .delay(for: .seconds(delay), scheduler: RunLoop.main, options: .none)
             .flatMap { _ in publisher }
             .assign(to: \.response, on: self)
             .store(in: &subscriptions)
