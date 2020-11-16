65d606cac313fd34aedd8ccd3e27dfd0d790368c
foolbear <greatfoolbear@gmail.com>
2020-11-17 00:17:42 +0800

    Default SwiftUI project.

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by foolbear on 2020/11/17.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
